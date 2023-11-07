a089158a56ab317b0d9deb6ef3d90fc738b7b37d
Yakiv87 <130331039+Yakiv87@users.noreply.github.com>
2023-11-07 00:21:37 -0500

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
 import css from 'index.css';
 
 import { Profile } from './Profile/Profile';
-import user from './Profile/user';
+import user from './Profile/user.json';
 
 import { Statistics } from './Statistics/Statistics';
-import data from './Statistics/data';
+import data from './Statistics/data.json';
 
 import { FriendList } from './FriendList/FriendList';
-import friends from './FriendList/friends';
+import friends from './FriendList/friends.json';
 
 import { TransactionHistory } from './TransactionHistory/TransactionHistory';
-import transactions from './TransactionHistory/transactions';
+import transactions from './TransactionHistory/transactions.json';
 
 export const App = () => {
   return (
